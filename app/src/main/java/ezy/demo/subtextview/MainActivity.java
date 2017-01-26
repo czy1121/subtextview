8fbcdd300e99620f4351d87839fb9f07fbbe371c
czy1121 <249736246@qq.com>
2017-01-26 17:19:47 +0800

    修改包名

--- app/src/main/java/ezy/demo/subtextview/MainActivity.java
+++ app/src/main/java/ezy/demo/subtextview/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.github.czy1121.subtextview.demo.R;
+
 
 import ezy.ui.veiw.SubTextView;
 
